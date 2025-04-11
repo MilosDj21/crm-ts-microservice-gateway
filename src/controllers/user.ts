973bb489744e5c0a8a7e43d7e636ca554d0b3b91
Milos <m.djuka@hotmail.com>
2025-04-11 22:52:21 +0200

    feat: Implement createUser controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -17,11 +17,31 @@ const getUserById = async (
 
 const getUsers = async (req: Request, res: Response, next: NextFunction) => {};
 
-const createUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {};
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password, firstName, lastName, roleIds } = req.body;
+  const profileImage = req.file;
+  try {
+    if (!email || !password || !firstName || !lastName || !roleIds)
+      throw new BadRequestError("All fields must be filled");
+
+    const userService = new UserService();
+    const imageQr = await userService.saveOne(
+      email,
+      password,
+      firstName,
+      lastName,
+      profileImage ? profileImage.path : "",
+      roleIds,
+    );
+
+    res.status(200).json({
+      data: imageQr,
+      message: "Added Successfully!",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 const updateUser = async (
   req: Request,
