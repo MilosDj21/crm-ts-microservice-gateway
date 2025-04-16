e13016337128d2409adafdb9b8e35d7c25d44f56
Milos <m.djuka@hotmail.com>
2025-04-16 20:37:03 +0200

    feat: Add role routes

---
+++ src/routes/roleRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+import {
+  getRoleById,
+  getRoles,
+  createRole,
+  updateRole,
+  deleteRole,
+} from "../controllers/roleController";
+import { isAdmin, verifyToken } from "../middlewares/auth";
+
+const router = Router();
+
+router.use(verifyToken);
+router.use(isAdmin);
+
+router.get("/:id", getRoleById);
+router.get("/", getRoles);
+router.post("/", createRole);
+router.patch("/:id", updateRole);
+router.delete("/:id", deleteRole);
+
+export default router;
